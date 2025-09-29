ae4c65bc1edf949526465735f21182754dfdd799
PettLe <90179827+PettLe@users.noreply.github.com>
2025-09-29 13:12:01 +0300

    complete 06 - 11

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,20 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...items) {
+    let remove = []
+    for (i in items) {
+        remove.push(items[i])
+    }
+
+    let newArr = arr.filter(value => remove.indexOf(value) < 0)
+    console.log(newArr)
+    return newArr
+//     for (i in items) {
+//         console.log("Tämä item on " + items[i])
+//         arr.map(value => 
+//             {if (value !== items[i]) 
+//                 {newArr.push(value)}})
+//         console.log("New array is " + newArr)
+//             }
+//     return newArr
 };
 
 // Do not edit below this line
