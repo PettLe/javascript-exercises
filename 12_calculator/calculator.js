6ff6df74ab3af01d220a1331595f18c25c58c179
PettLe <90179827+PettLe@users.noreply.github.com>
2025-10-03 09:23:42 +0300

    Complete exercise 12

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  if (array.length == 0) {
+    return 0
+  } else {
+	return array.reduce((total, num) => total += num)
+  }
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((total, num) => total *= num)
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return Math.pow(a, b)
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+  let tempArray = []
+  if (number <= 1) {
+    return 1
+  } else {
+	  for (let i = number; i > 1; i--) {
+      tempArray.push(i)
+    }
+}
+  return tempArray.reduce((total, num) => total *= num)
 };
 
 // Do not edit below this line
