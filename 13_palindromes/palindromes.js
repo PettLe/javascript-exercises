1fcb81b7518e048004a49f0a44a257a7cf3973fc
PettLe <90179827+PettLe@users.noreply.github.com>
2025-10-03 10:43:04 +0300

    complete exercises 13-16

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (text) {
+    let original = text.replace(/\s+/g, "").replace(/[^\w\s]/gi,"").toLowerCase()
+    let backwards = original.split("").reverse().join("")
+    console.log(original)
+    console.log(backwards)
+    return original == backwards
 };
 
 // Do not edit below this line
